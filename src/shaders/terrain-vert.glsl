05f67328bd0ee5bed351020c6ec1c192bd032cf1
Catherine Yang <catyang@seas.upenn.edu>
2019-01-31 03:31:23 -0500

    height field with ice caps

--- src/shaders/terrain-vert.glsl
+++ src/shaders/terrain-vert.glsl
@@ -1,6 +1,5 @@
 #version 300 es
 
-
 uniform mat4 u_Model;
 uniform mat4 u_ModelInvTr;
 uniform mat4 u_ViewProj;
@@ -15,6 +14,8 @@ out vec4 fs_Nor;
 out vec4 fs_Col;
 
 out float fs_Sine;
+out float fs_Height;
+
 
 float random1( vec2 p , vec2 seed) {
   return fract(sin(dot(p + seed, vec2(127.1, 311.7))) * 43758.5453);
@@ -28,11 +29,47 @@ vec2 random2( vec2 p , vec2 seed) {
   return fract(sin(vec2(dot(p + seed, vec2(311.7, 127.1)), dot(p + seed, vec2(269.5, 183.3)))) * 85734.3545);
 }
 
+float interpNoise2D(float x, float y) { //from slides
+    float intX = floor(x);
+    float fractX = fract(x);
+    float intY = floor(y);
+    float fractY = fract(y);
+
+    float v1 = random1(vec2(intX, intY), vec2(1.f, 1.f));
+    float v2 = random1(vec2(intX + 1.0f, intY), vec2(1.f, 1.f));
+    float v3 = random1(vec2(intX, intY + 1.0f), vec2(1.f, 1.f));
+    float v4 = random1(vec2(intX + 1.0, intY + 1.0), vec2(1.f, 1.f));
+
+    float i1 = mix(v1, v2, fractX);
+    float i2 = mix(v3, v4, fractX);
+
+    return mix(i1, i2, fractY);
+}
+
+// Create a height field based on summed fractal noise
+// Adjust distribution of noise values so they are biased to various height values, 
+// or even radically remap height values entirely! 
+float fbm(float x, float y) { //from slides
+  float total = 0.0f;
+  float persistence = 0.5f;
+  float octaves = 10.0;
+
+  for (float i = 0.0; i < octaves; i = i + 1.0) {
+      float freq = pow(2.0f, i);
+      float amp = pow(persistence, i);
+      total += interpNoise2D(x * freq, y * freq) * amp;
+  }
+  return total;
+}
+
 void main()
 {
-  fs_Pos = vs_Pos.xyz;
-  fs_Sine = (sin((vs_Pos.x + u_PlanePos.x) * 3.14159 * 0.1) + cos((vs_Pos.z + u_PlanePos.y) * 3.14159 * 0.1));
-  vec4 modelposition = vec4(vs_Pos.x, fs_Sine * 2.0, vs_Pos.z, 1.0);
+  float elevation = fbm((vs_Pos.x + u_PlanePos.x) / 8.0, (vs_Pos.z + u_PlanePos.y) / 8.0);
+  elevation = pow(elevation, 3.0) + floor(elevation); // or multiply
+  // elevation = pow(elevation, 5.0) * floor(elevation) * random1(vec2(vs_Pos.z + u_PlanePos.y), vec2(100.f, 200.f)) * random1(vec2(vs_Pos.x + u_PlanePos.x), vec2(1.f, 1.f)); // melting ice caps
+  fs_Height = elevation;
+  fs_Pos = vec3(vs_Pos.x, elevation, vs_Pos.z);
+  vec4 modelposition = vec4(vs_Pos.x, elevation, vs_Pos.z, 1.0);
   modelposition = u_Model * modelposition;
   gl_Position = u_ViewProj * modelposition;
 }
